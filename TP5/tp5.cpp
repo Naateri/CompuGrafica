9c8bc17b968f0d5bcc6276b5c0be7983b49fc1af
Renato Postigo <renato.postigo@ucsp.edu.pe>
2019-11-24 16:48:18 -0500

    TP5 - Check normals

--- TP5/tp5.cpp
+++ TP5/tp5.cpp
@@ -7,6 +7,7 @@
 #include <GL/glut.h>
 #include <fstream>
 #include <string>
+#include <vector>
 #include "TextureManager.h"
 
 using namespace std;
@@ -24,14 +25,17 @@ GLvoid window_display();
 GLvoid window_reshape(GLsizei width, GLsizei height);
 GLvoid window_key(unsigned char key, int x, int y);
 
-
 //variables para el gizmo
-float delta_x = 0.0; 
-float delta_y = 0.0;
+float delta_x = -49.0; 
+float delta_y = 16.0;
 float mouse_x, mouse_y;
-float var_x = 0.0;
-float var_z = -30.0;
+float var_x = 310.0;
+float var_z = -830.0;
 float step = 0; //0.0 Posicion inicial. 1.0 Traslacion. 2.0 Primera Rotacion(en y). 3.0 Segunda Rotacion (en x) 4.0 Ultima Rotacion (en z)
+
+bool light = true;
+bool normals = true;
+bool texture = true;
 
 typedef struct // Definicion de un punto del terreo
 {
@@ -39,6 +43,30 @@ typedef struct // Definicion de un punto del terreo
 	GLfloat nx, ny, nz; // Coordenadas de la normal
 	GLfloat x, height, z; // posición del vértice
 } PuntoTerreno;
+
+vector<GLfloat> TriangleNormal(PuntoTerreno A, PuntoTerreno B, PuntoTerreno C){
+	GLfloat nx, ny, nz, ax, ay, az, bx, by, bz;
+	
+	ax = B.x - A.x;
+	ay = B.height - A.height;
+	az = B.z - A.z;
+	
+	bx = C.x - A.x;
+	by = C.height - A.height;
+	bz = C.z - A.z;
+	
+	nx = (ay * bz) - (by * az);
+	ny = (bx * az) - (ax * bz);
+	nz = (ax * by) - (bx * ay);
+	
+	GLfloat sum = sqrt(pow(nx,2) + pow(ny,2) + pow(nz,2));
+	nx /= sum; ny /= sum; nz /= sum;
+	
+	vector<GLfloat> normal;
+	normal.push_back(nx); normal.push_back(ny); normal.push_back(nz);
+	
+	return normal;
+}
 
 class Terreno{
 private:
@@ -51,7 +79,8 @@ public :
 	Terreno(){
 		lista_puntos = new PuntoTerreno[102*101];
 		lista_indices = new GLuint[102*101*6];
-		dist_x = 50.0f; dist_z = 50.0f;
+		dist_x = 50.0f; 
+		dist_z = 50.0f;
 		nb_pt_x = 101;
 		nb_pt_z = 101;
 	}
@@ -65,14 +94,23 @@ void Terreno::build_triangles(){
 	int cur_index = 0;
 	for(int i = 0; i < nb_pt_z-1; i++){
 		for (int j = 0; j < nb_pt_x-1; j++){
-			lista_indices[cur_index] = i*nb_pt_x + j;
+			int position = i*nb_pt_x + j;
+			//right
+			/*lista_indices[cur_index] = i*nb_pt_x + (j+1);
 			lista_indices[cur_index+1] = (i+1)*nb_pt_x + j;
-			lista_indices[cur_index+2] = (i+1)*nb_pt_x + (j+1);
+			lista_indices[cur_index+2] = (i+1)*nb_pt_x + (j+1);*/
+			lista_indices[cur_index] = position + 1;
+			lista_indices[cur_index+1] = position + nb_pt_x;
+			lista_indices[cur_index+2] = position + nb_pt_x + 1;
 			cur_index += 3;
 			
-			lista_indices[cur_index] = i*nb_pt_x + j;
+			//left
+			/*lista_indices[cur_index] = i*nb_pt_x + j;
 			lista_indices[cur_index+1] = i*nb_pt_x + (j+1);
-			lista_indices[cur_index+2] = (i*nb_pt_x + 1) + (j + 1);
+			lista_indices[cur_index+2] = (i+1)*nb_pt_x + (j);*/
+			lista_indices[cur_index] = position;
+			lista_indices[cur_index+1] = position + 1;
+			lista_indices[cur_index+2] = position + nb_pt_x;
 			
 			cur_index += 3;
 		}
@@ -80,10 +118,249 @@ void Terreno::build_triangles(){
 	cout << "triangle vertices " << cur_index << endl;
 }
 
+void Terreno::computeNormals(){
+	int cur_index = 0;
+	PuntoTerreno cur_pt;
+	PuntoTerreno TriA, TriB, TriC;
+	vector<GLfloat> normals;
+	
+	GLfloat cur_nx, cur_ny, cur_nz;
+	
+	for(int i = 0; i < nb_pt_z; i++){
+		for (int j = 0; j < nb_pt_x; j++){
+			
+			cur_nx = cur_ny = cur_nz = 0.0;
+			
+			cur_pt = lista_puntos[cur_index];
+			normals.clear();
+			if (j == 0){ //points at the left
+				if (i == 0){  //top-left corner
+					TriA = lista_puntos[0];
+					TriB = lista_puntos[1];
+					TriC = lista_puntos[nb_pt_x];
+					
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_pt.nx = normals[0];
+					cur_pt.ny = normals[1];
+					cur_pt.nz = normals[2];
+				} else if (i == nb_pt_z - 1){ //bottom-left corner
+					//two triangles
+					//above
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index - nb_pt_x];
+					TriC = lista_puntos[cur_index - nb_pt_x + 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//right
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index + 1];
+					TriC = lista_puntos[cur_index - nb_pt_x + 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					cur_nx = cur_nx / 2; cur_ny = cur_ny / 2; cur_nz = cur_nz / 2;
+					
+					cur_pt.nx = cur_nx; cur_pt.ny = cur_ny; cur_pt.nz = cur_nz;
+					
+				} else { //left points, 3 triangles to consider
+					//above-triangle
+					TriA = lista_puntos[cur_index - nb_pt_x];
+					TriB = lista_puntos[cur_index - nb_pt_x + 1];
+					TriC = lista_puntos[cur_index];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//above-right-triangle
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index - nb_pt_x + 1];
+					TriC = lista_puntos[cur_index + 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//below triangle
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index + 1];
+					TriC = lista_puntos[cur_index + nb_pt_x];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					cur_nx = cur_nx / 3; cur_ny = cur_ny / 3; cur_nz = cur_nz / 3;
+					
+					cur_pt.nx = cur_nx; cur_pt.ny = cur_ny; cur_pt.nz = cur_nz;
+				}
+			} else if (i == 0){ //first row
+				if (j == nb_pt_x - 1){ //top right corner
+					//two triangles
+					//below
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index + nb_pt_x];
+					TriC = lista_puntos[cur_index + nb_pt_x - 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//left
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index - 1];
+					TriC = lista_puntos[cur_index + nb_pt_x - 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					cur_nx = cur_nx / 2; cur_ny = cur_ny / 2; cur_nz = cur_nz / 2;
+					
+					cur_pt.nx = cur_nx; cur_pt.ny = cur_ny; cur_pt.nz = cur_nz;
+				} else { //rest of first row
+					//left triangle
+					TriA = lista_puntos[cur_index]; //three triangles
+					TriB = lista_puntos[cur_index - 1];
+					TriC = lista_puntos[cur_index + nb_pt_x - 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//below
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index + nb_pt_x - 1];
+					TriC = lista_puntos[cur_index + nb_pt_x];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//right
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index + 1];
+					TriC = lista_puntos[cur_index + nb_pt_x];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					cur_nx = cur_nx / 3; cur_ny = cur_ny / 3; cur_nz = cur_nz / 3;
+					
+					cur_pt.nx = cur_nx; cur_pt.ny = cur_ny; cur_pt.nz = cur_nz;
+				}
+			} else if (j == nb_pt_x - 1){ //right side
+				if (i == nb_pt_z - 1){ //bottom-right corner
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index - 1];
+					TriC = lista_puntos[cur_index - nb_pt_x];
+					
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_pt.nx = normals[0];
+					cur_pt.ny = normals[1];
+					cur_pt.nz = normals[2];
+				} else { //rest of right side
+					//three triangles
+					//above
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index - nb_pt_x];
+					TriC = lista_puntos[cur_index - 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//left
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index - 1];
+					TriC = lista_puntos[cur_index + nb_pt_x - 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					//below
+					TriA = lista_puntos[cur_index];
+					TriB = lista_puntos[cur_index + nb_pt_x];
+					TriC = lista_puntos[cur_index + nb_pt_x - 1];
+					normals = TriangleNormal(TriA, TriB, TriC);
+					cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+					
+					cur_nx = cur_nx / 3; cur_ny = cur_ny / 3; cur_nz = cur_nz / 3;
+					
+					cur_pt.nx = cur_nx; cur_pt.ny = cur_ny; cur_pt.nz = cur_nz;
+				}
+			} else if (i == nb_pt_z - 1){ //last row
+				//three triangles
+				//left
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index - 1];
+				TriC = lista_puntos[cur_index - nb_pt_x];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				//above
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index - nb_pt_x];
+				TriC = lista_puntos[cur_index - nb_pt_x + 1];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				//right
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index + 1];
+				TriC = lista_puntos[cur_index - nb_pt_x + 1];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				cur_nx = cur_nx / 3; cur_ny = cur_ny / 3; cur_nz = cur_nz / 3;
+				
+				cur_pt.nx = cur_nx; cur_pt.ny = cur_ny; cur_pt.nz = cur_nz;
+			} else { //regular case: 6 triangles to consider
+				//left-above triangle
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index - 1];
+				TriC = lista_puntos[cur_index - nb_pt_x];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				//above triangle
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index - nb_pt_x];
+				TriC = lista_puntos[cur_index - nb_pt_x + 1];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				//right-above triangle
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index - nb_pt_x + 1];
+				TriC = lista_puntos[cur_index + 1];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				//right-below Triangle
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index + 1];
+				TriC = lista_puntos[cur_index + nb_pt_x];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				//below Triangle
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index + nb_pt_x];
+				TriC = lista_puntos[cur_index + nb_pt_x - 1];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				//left-below triangle 
+				TriA = lista_puntos[cur_index];
+				TriB = lista_puntos[cur_index - 1];
+				TriC = lista_puntos[cur_index + nb_pt_x - 1];
+				normals = TriangleNormal(TriA, TriB, TriC);
+				cur_nx += normals[0]; cur_ny += normals[1]; cur_nz += normals[2];
+				
+				cur_nx = cur_nx / 6; cur_ny = cur_ny / 6; cur_nz = cur_nz / 6;
+				
+				cur_pt.nx = cur_nx; cur_pt.ny = cur_ny; cur_pt.nz = cur_nz;
+			}
+			
+			lista_puntos[cur_index] = cur_pt;
+			
+			cur_index++;
+		}
+		
+	}
+	cout << "cur_index: " << cur_index << endl;
+}
+
 bool Terreno::load(char* filename){
 	ifstream myfile (filename);
 	string line, cur_num;
 	PuntoTerreno* temp;
+	
+	int x = 0, z = 0;
 	
 	float start_x = 0.0f, start_z = 0.0f;
 	float height;
@@ -96,6 +373,7 @@ bool Terreno::load(char* filename){
 			if (cur_line++ == 0) continue;
 			//cout << line << endl;
 			start_x = 0.0f;
+			x = 0;
 			
 			cur_num.clear();
 			
@@ -106,19 +384,26 @@ bool Terreno::load(char* filename){
 					temp->x = start_x;
 					temp->height = height;
 					temp->z = start_z;
+					
+					temp->t = (float)x/101.0f;
+					temp->s = (float)z/101.0f;
+					
 					lista_puntos[cur_line-1] = *temp;
 					
 					//last_cur_num = cur_num;
 					
 					cur_num.clear();
 					cur_line++;
+			
 					start_x += 50.0f;
+					x++;
 				} else {
 					cur_num += line[i];
 				}
 			}
 			
 			start_z += dist_z;
+			z++;
 		}
 		cout << "total points: " << cur_line-1 << endl;
 		
@@ -131,14 +416,31 @@ bool Terreno::load(char* filename){
 }
 
 void Terreno::display(){
+	
+	GLfloat ambient[] = {0.0f, 0.1f, 0.0f, 1.0f};
+	GLfloat difuse[] = {0.0f, 0.5f, 0.0f, 1.0f};
+	GLfloat specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	GLfloat emission[] = {0.4f, 0.4f, 0.4f, 1.0f};
+	GLfloat shininess = 50.0f;
+	
+	glEnable(GL_COLOR_MATERIAL);
+	
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, difuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION, emission);
+	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+	
+	
 	int count = 101*101*6;
-	glColor3f(0.0f, 0.0f, 1.0f);
+	
 	glInterleavedArrays(GL_T2F_N3F_V3F, sizeof(PuntoTerreno), &lista_puntos[0].s);
 	glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, (void*)lista_indices);
 }
 
 
 Terreno* terreno;
+GLuint terrain_texture;
 
 ///////////////////////////////////////////////////////////////////////////////
 //(1)
@@ -148,22 +450,22 @@ GLvoid callback_special(int key, int x, int y)
 	switch (key)
 	{
 	case GLUT_KEY_UP:
-		var_z += 10.0;
+		var_z += 20.0;
 		glutPostRedisplay();
 		break;
 
 	case GLUT_KEY_DOWN:
-		var_z -= 10.0;
+		var_z -= 20.0;
 		glutPostRedisplay();
 		break;
 
 	case GLUT_KEY_LEFT:
-		var_x += 5.0;
+		var_x += 20.0;
 		glutPostRedisplay();
 		break;
 
 	case GLUT_KEY_RIGHT:
-		var_x -= 5.0;
+		var_x -= 20.0;
 		glutPostRedisplay();
 		break;
 
@@ -217,14 +519,14 @@ int main(int argc, char **argv)
 	glutInitWindowSize(800, 800);
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("TP 5");
-
-
+	
 	initGL();
 	init_scene();
 
 	terreno = new Terreno;
-	
+	if (texture) terrain_texture = TextureManager::Inst()->LoadTexture("fontvieille.tga", GL_BGR_EXT, GL_RGB);
 	terreno->load("fontvieille.txt");
+	if (normals) terreno->computeNormals();
 	
 	glutDisplayFunc(&window_display);
 	glutReshapeFunc(&window_reshape);
@@ -246,12 +548,10 @@ int main(int argc, char **argv)
 	return 1;
 }
 
-
-
 GLvoid initGL()
 {
 
-	/*GLfloat position[] = { 2500.0f, 100000.0f, 2500.0f, 0.0 };
+	GLfloat position[] = { 2500.0f, 300.0f, 2500.0f, 0.0 };
 	GLfloat light0Ambient[] = {0.3f, 0.3f, 0.3f, 1.0f};
 	GLfloat light0Diffuse[] = {5.0f, 5.0f, 5.0f, 1.0f};
 	GLfloat light0Specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
@@ -261,11 +561,17 @@ GLvoid initGL()
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light0Diffuse);
 	glLightfv(GL_LIGHT0, GL_SPECULAR, light0Specular);
 	
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);*/
+	if (light){
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+	}
+	
+	glEnable(GL_TEXTURE_2D);
+	glShadeModel(GL_SMOOTH);
 	
 	glEnable(GL_DEPTH_TEST);
-
+	glDepthFunc(GL_LEQUAL);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
 	glClearColor(RED, GREEN, BLUE, ALPHA);
 }
@@ -285,7 +591,7 @@ GLvoid window_display()
 	glTranslatef(var_x, 0.0, var_z);
 	glRotatef(delta_x, 0.0, 1.0, 0.0);
 	glRotatef(delta_y, 1.0, 0.0, 0.0);
-		
+	
 	terreno->display();
 
 	glutSwapBuffers();
